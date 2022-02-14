c17e42edf75537ff115358e8725a197d167164b4
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-02-14 08:43:10 -0500

    initial commit

---
+++ SlotMachine/SlotMachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotMachineApp.swift
+//  SlotMachine
+//
+//  Created by Thomas Cowern New on 2/14/22.
+//
+
+import SwiftUI
+
+@main
+struct SlotMachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
