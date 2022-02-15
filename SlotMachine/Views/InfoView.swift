bf17e4f895a6cbb465764f1f590123a5a8f3f770
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-02-15 07:01:30 -0500

    added info view

---
+++ SlotMachine/Views/InfoView.swift
@@ -0,0 +1,20 @@
+//
+//  InfoView.swift
+//  SlotMachine
+//
+//  Created by Thomas Cowern New on 2/15/22.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoView()
+    }
+}
